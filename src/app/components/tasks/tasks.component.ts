7bc6f50b8baaab49a99f95f5c1d84553f1b64c05
Veronica <veronica.ardilao@outlook.es>
2021-07-30 12:48:30 -0500

    se creo el servicio task y se conecto al componente task tipando con un observable

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/Task';
-import { TASKS } from 'src/app/mock-task';
+import { TaskService } from 'src/app/services/task.service';
+
 
 @Component({
   selector: 'app-tasks',
@@ -9,11 +10,12 @@ import { TASKS } from 'src/app/mock-task';
 })
 export class TasksComponent implements OnInit {
 
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService: TaskService ) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks)=> (this.tasks = tasks));
   }
 
 }
