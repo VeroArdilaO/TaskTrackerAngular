5425db34c84724fcc2c8466bd349bb689240df04
Veronica <veronica.ardilao@outlook.es>
2021-07-30 15:09:41 -0500

    se creo el mock json server para crear la api y conectarla por medio de HttpClient

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from   '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-task';
@@ -8,10 +9,11 @@ import { TASKS } from 'src/app/mock-task';
 })
 export class TaskService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/tasks'
+
+  constructor(private http: HttpClient ) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks =  of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
